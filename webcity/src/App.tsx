8cbb9f11d83767ded580f9ab25b1718cf51d9730
Ethan Ma <etonc3572@gmail.com>
2025-02-21 15:50:06 -0600

    New Style

--- webcity/src/App.tsx
+++ webcity/src/App.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Flex, MantineProvider, Box, Button} from '@mantine/core';
+import logo from './image.png';
+import { MantineProvider, Box, Button, Title, SimpleGrid} from '@mantine/core';
 import Navbar from "./Navbar"
 import './App.css';
+import { redirect } from 'react-router-dom';
 
 function App() {
+  const handleButtonRedirect = (url : string) => {
+    window.location.href = url;
+  } 
+    
   return (
-    <div className="App">
+    <MantineProvider>
       <header className="App-header">
-        <MantineProvider>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Box>
-              Hi, my name is Ethan and I'm currently pursuing a computer science degree at UIUC
-          </Box>
-          <Box maw="1000px" style={{ display: 'flex', gap: '16px'}}>
-              
-              <a
-                className="App-link"
-                href="https://github.com/EthM370"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                github Link
-              </a>
-              <a
-                className="App-link"
-                href="https://www.linkedin.com/in/ethan-ma-8207a6327"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                linkedin Link
-              </a>
-          </Box>
-        </MantineProvider>
+            <img src={logo} className="App-logo" alt="logo" />
       </header>
-    </div>
+
+      <Box style={{margin: "16px", maw:"500px", alignContent:'center'}}>
+        <Title order={1}>Hi, my name is Ethan and this is my website!</Title>
+        <Title order={2}>I'm currently pursuing a computer science degree at UIUC </Title>
+          
+        <SimpleGrid cols={1}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </SimpleGrid>
+        <Button className="linkButton" onClick={() => {handleButtonRedirect("https://github.com/EthM370")}}>
+          GitHub Link
+          </Button>    
+        <Button className="linkButton" onClick={() => {handleButtonRedirect("https://www.linkedin.com/in/ethan-ma-8207a6327")}}>
+          Linkedin Link
+        </Button>      
+      </Box>
+    </MantineProvider>
   );
 }
 
